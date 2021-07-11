3426f0b3f12355659f9b859e2474d7bbd4724576
cristan <13849765+cristan2@users.noreply.github.com>
2021-07-12 01:18:23 +0300

    Added rexif library and created sub-subdirs by device name

--- imgsorter/src/main.rs
+++ imgsorter/src/main.rs
@@ -1,20 +1,19 @@
-use std::path::Path;
+use std::path::PathBuf;
 use std::{fs, env};
-use std::error::Error;
-use std::time::UNIX_EPOCH;
 use chrono::{DateTime, Utc};
-use std::fs::{ReadDir, DirEntry, DirBuilder};
-use std::collections::HashMap;
+use std::fs::{DirEntry, DirBuilder};
+// use std::collections::HashMap;
+use rexif::{ExifEntry, ExifTag};
 
-// arg for paths
-// arg for min count for move
-// arg flag for logfile
-// copy/move file only if it's file
-// subdirs by device name
-// img count in dir name
-// change copy to rename
-// cleanup
-// undo ?
+// [ ] arg for paths
+// [ ] arg for min count for move
+// [ ] arg flag for logfile
+// [ ] copy/move file only if it's file
+// [x] subdirs by device name
+// [ ] img count in dir name
+// [ ] change copy to rename
+// [ ] cleanup
+// [ ] undo ?
 
 fn main() {
 
@@ -22,45 +21,47 @@ fn main() {
 
     println!("current working directory = {}", cwd.display());
 
-    // let base_path = Path::new(".")
-    let base_path = cwd
-        .join("test_pics");
+    // TODO temp, ar trebui sa fie argument sau doar current dir
+    // let path_cwd = Path::new(".")
+    let path_cwd: PathBuf = cwd.join("test_pics");
 
-    let file_list = fs::read_dir(&base_path).unwrap();
+    // TODO filter files, images
+    let file_list = fs::read_dir(&path_cwd).unwrap();
 
-    let mut new_dir_tree: HashMap<String, Vec<DirEntry>> = HashMap::new();
+    // let mut new_dir_tree: HashMap<String, Vec<DirEntry>> = HashMap::new();
 
+    // TODO add printout no of files
     // iterate files, read modified date and create dirs
     for entry in file_list {
         // dbg!(entry);
-        let file = entry.unwrap();
+        let file = entry.unwrap(); // TODO unwrap?
 
-        let formatted_time = get_modified_time(&file);
+        let formatted_time: Option<String> = get_modified_time(&file);
+        let device_name: Option<String> = get_device_name(&file);
 
-        let sub_dir = base_path.join(&formatted_time);
+        // create image date subdir path
+        if let Some(date) = formatted_time {
+            let mut target_subdir = path_cwd.join(&date);
 
-        let dir_created = DirBuilder::new()
-            .recursive(true) // create subdirs if necessary; don't return Err if file exists
-            .create(&sub_dir);
-        // dbg!(dir_created);
+            // attach device name subdir path
+            if let Some(device) = device_name {
+                target_subdir.push(&device);
+            }
 
-        let new_file_path = sub_dir.join(&file.file_name());
+            create_subdir_if_required(&target_subdir, &path_cwd);
 
+            // attach file path
+            // TODO create new path variable?
+            target_subdir.push(&file.file_name());
 
-        match fs::copy(&file.path(), &new_file_path) {
-            Ok(copy_res) =>
-                println!("{} -> {} ... ok",
-                         // &file.file_name().to_str().unwrap(),
-                         &file.path().strip_prefix(&cwd).unwrap().display(),
-                         // &new_file_path.to_str().unwrap()),
-                         &new_file_path.strip_prefix(&cwd).unwrap().display()),
-            Err(err) =>
-                println!("{:?}: ERROR {:?}", &file.file_name(), err)
-        };
+            // copy file
+            copy_file_if_not_exists(&file, &target_subdir, &path_cwd);
+        }
 
-        let sub_dir = new_dir_tree.entry(formatted_time).or_insert(Vec::new());
-        sub_dir.push(file);
+        /*let sub_dir = new_dir_tree.entry(formatted_time).or_insert(Vec::new());
+        sub_dir.push(file);*/
     }
+
 
 /*    new_dir_tree.iter().for_each(|(dir_name, dir_files)|{
         println!("{} ({})", dir_name, dir_files.len());
@@ -72,9 +73,102 @@ fn main() {
     })*/
 }
 
-fn get_modified_time(file: &DirEntry) -> String {
-    let metadata = file.metadata().unwrap();
-    let created = metadata.modified().unwrap();
-    let datetime: DateTime<Utc> = created.into(); // 2021-06-05T16:26:22.756168300Z
-    datetime.format("%Y.%m.%d").to_string()
+fn copy_file_if_not_exists(file: &DirEntry, target_subdir: &PathBuf, path_cwd: &PathBuf) {
+    let file_copy_status = if target_subdir.exists() {
+        "already exists"
+    } else {
+        match fs::copy(file.path(), target_subdir) {
+            Ok(_) =>
+                "ok",
+            Err(err) =>
+                // println!("File copy error: {:?}: ERROR {:?}", &file.file_name(), err)
+                // TODO add error info
+                "ERROR"
+        }
+    };
+
+    println!("Copying {} -> {} ... {}",
+             // &file.file_name().to_str().unwrap(),
+             file.path().strip_prefix(path_cwd).unwrap().display(),
+             // &new_file_path.to_str().unwrap()),
+             target_subdir.strip_prefix(path_cwd).unwrap().display(),
+             file_copy_status);
+}
+
+fn create_subdir_if_required(target_subdir: &PathBuf, path_cwd: &PathBuf) {
+    if target_subdir.exists() {
+        // TODO maybe log it exists?
+        // println!("target dir exists: {}: {}",
+        //          &target_subdir.strip_prefix(&path_cwd).unwrap().display(),
+        //          &target_subdir.exists());
+        // "already exists"
+    } else {
+        let subdir_creation = DirBuilder::new()
+            // create subdirs if necessary; don't return Err if file exists
+            .recursive(true)
+            .create(target_subdir);
+
+        match subdir_creation {
+            Ok(_) => {
+                println!("(Created subdirectory {})",
+                         target_subdir.strip_prefix(&path_cwd).unwrap().display());
+            },
+            Err(e) =>
+            // TODO handle dir creation fail
+                println!("Failed to create subdirectory {}: {:?}",
+                         target_subdir.strip_prefix(&path_cwd).unwrap().display(),
+                         e.kind())
+        }
+    };
+}
+
+fn get_modified_time(file: &DirEntry) -> Option<String> {
+    let modified_time = if let Ok(metadata) = file.metadata() {
+        match metadata.modified() {
+            Ok(created) => {
+                let datetime: DateTime<Utc> = created.into(); // 2021-06-05T16:26:22.756168300Z
+                Some(datetime.format("%Y.%m.%d").to_string())
+            },
+            Err(_) =>
+                None
+        }
+    } else {
+        None
+    };
+
+    modified_time
+}
+
+fn get_device_name(file: &DirEntry) -> Option<String> {
+    let file_name = file.path();
+
+    // TODO parse_file produces warnings to stderr
+    match rexif::parse_file(&file_name) {
+
+        Ok(exif) => {
+            let model = &exif.entries.iter()
+                .filter(|exif_entry| {
+                    exif_entry.tag == ExifTag::Model})
+                .map(|e| e)
+                .collect::<Vec<&ExifEntry>>();
+
+            return match model.len() {
+                0 => None,
+                _len => {
+                    let model_name = model.get(0).map_or(None, |entry| {
+                        let s = entry.value.to_string().trim().to_string();
+                        Some(s)
+                    });
+                    model_name
+                }
+            }
+        },
+
+        Err(e) => {
+            // dbg!(e);
+            println!("Error in {:?}: {}", &file_name, e.to_string());
+            None
+        }
+    }
+
 }
