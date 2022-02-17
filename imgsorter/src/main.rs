28640d3d6ec281ade09f7e58f5ab206053822e92
cristan <13849765+cristan2@users.noreply.github.com>
2022-02-18 00:09:01 +0200

    Added processing stats

--- imgsorter/src/main.rs
+++ imgsorter/src/main.rs
@@ -2,11 +2,10 @@ use std::path::{PathBuf, Path};
 use std::{fs, env};
 use chrono::{DateTime, Utc};
 use std::fs::{DirEntry, DirBuilder, File, Metadata};
-use std::collections::HashMap;
 use rexif::{ExifEntry, ExifTag, ExifResult};
 use std::io::{Read, Seek, SeekFrom};
 
-const DBG_ON: bool = true;
+const DBG_ON: bool = false;
 const NO_DATE: &'static str = "no date";
 
 #[derive(Debug)]
@@ -16,11 +15,55 @@ pub enum FileType {
     Video,
 }
 
+#[derive(Debug)]
+pub struct FileStats {
+    files_total: i32,
+    img_moved: i32,
+    img_skipped: i32,
+    vid_moved: i32,
+    vid_skipped: i32,
+    unknown_skipped: i32,
+    dirs_skipped: i32,
+    dirs_created: i32,
+    error_file_move: i32,
+    error_dir_creation: i32,
+}
+
+impl FileStats {
+    pub fn new() -> FileStats {
+        FileStats {
+            files_total: 0,
+            img_moved: 0,
+            img_skipped: 0,
+            vid_moved: 0,
+            vid_skipped: 0,
+            unknown_skipped: 0,
+            dirs_skipped: 0,
+            dirs_created: 0,
+            error_file_move: 0,
+            error_dir_creation: 0,
+        }
+    }
+
+    pub fn files_total(&mut self) { self.files_total += 1}
+    pub fn img_moved(&mut self) { self.img_moved += 1}
+    pub fn img_skipped(&mut self) { self.img_skipped += 1}
+    pub fn vid_moved(&mut self) { self.vid_moved += 1}
+    pub fn vid_skipped(&mut self) { self.vid_skipped += 1}
+    pub fn unknown_skipped(&mut self) { self.unknown_skipped += 1}
+    pub fn dirs_skipped(&mut self) { self.dirs_skipped += 1}
+    pub fn dirs_created(&mut self) { self.dirs_created += 1}
+    pub fn error_file_move(&mut self) { self.error_file_move += 1}
+    pub fn error_dir_creation(&mut self) { self.error_dir_creation += 1}
+}
+
 fn main() -> Result<(), std::io::Error> {
+
+    let mut stats = FileStats::new();
 
     // TODO 5: unwrap here?
     let cwd = env::current_dir().unwrap();
-    println!("current working directory = {}", cwd.display());
+    println!("Current working directory is {}", cwd.display());
 
     // TODO 1a: temporary citim din ./test_pics, normal ar trebui sa fie argument sau doar current dir
     // let path_cwd = Path::new(".")
@@ -34,11 +77,13 @@ fn main() -> Result<(), std::io::Error> {
     let dir_contents = fs::read_dir(&cwd_path)?
         .into_iter()
         .filter_map(|entry| entry.ok())
+        // TODO 7b: we could skip collecting now, since we'll just iterate the collection later anyway
         .collect::<Vec<DirEntry>>();
 
-    // TODO 3a: add printout no of files
-    // iterate files, read modified date and create subdirs
+    // Iterate files, read modified date and create subdirs
     for dir_entry in dir_contents {
+
+        stats.files_total();
 
         if DBG_ON {
             /* Print whole entry */
@@ -51,7 +96,8 @@ fn main() -> Result<(), std::io::Error> {
         let dir_entry_metadata: Metadata = dir_entry.metadata()?;
 
         if dir_entry_metadata.is_dir() {
-            println!("{:?} is a directory, ignoring", &file_name);
+            println!("Skipping directory {:?}", &file_name);
+            stats.dirs_skipped();
         } else {
 
             let extension_opt: Option<String> = get_extension(&dir_entry);
@@ -74,39 +120,46 @@ fn main() -> Result<(), std::io::Error> {
             // TODO 6a: move instead of copy
             match file_type {
                 FileType::Image | FileType::Video => {
-
-                    let mut target_subdir = match formatted_time_opt {
-                        Some(date) =>
-                            target_dir_path.join(date),
-                        None =>
-                            target_dir_path.join(NO_DATE.to_string())
-                    };
-
-                    sort_file_to_subdir(dir_entry, &mut target_subdir, &cwd_path, device_name_opt)
+                    let mut target_subdir = target_dir_path
+                        .join(formatted_time_opt.unwrap_or(NO_DATE.to_string()));
+                    sort_file_to_subdir(dir_entry, &mut target_subdir, &cwd_path, device_name_opt, file_type, &mut stats)
                 },
-                FileType::Unknown =>
+                FileType::Unknown => {
+                    stats.unknown_skipped();
                     println!("Skipping unknown file {:?}", &file_name)
+                }
             }
         }
     }
 
+    // Print final stats
+    dbg!(&stats);
+
     Ok(())
 }
 
-fn print_file_list(dir_tree: HashMap<String, Vec<DirEntry>>) {
-    dir_tree.iter().for_each(|(dir_name, dir_files)|{
-        println!("{} ({})", dir_name, dir_files.len());
-        for file in dir_files {
-            let filename = &file.file_name();
-            println!("| + {}", filename.to_str().unwrap());
-        }
-        // dbg!(dir_files);
-    })
-}
+// TODO 6c: unused, do we need this?
+// fn print_file_list(dir_tree: HashMap<String, Vec<DirEntry>>) {
+//     dir_tree.iter().for_each(|(dir_name, dir_files)|{
+//         println!("{} ({})", dir_name, dir_files.len());
+//         for file in dir_files {
+//             let filename = &file.file_name();
+//             println!("| + {}", filename.to_str().unwrap());
+//         }
+//         // dbg!(dir_files);
+//     })
+// }
 
 /// Move the file to a subdirectory named after the file date
 /// Optionally, create additional subdir based on device name
-fn sort_file_to_subdir(file: DirEntry, target_subdir: &mut PathBuf, cwd_path: &PathBuf, device_name_opt: Option<String>) {
+fn sort_file_to_subdir(
+    file: DirEntry,
+    target_subdir: &mut PathBuf,
+    cwd_path: &PathBuf,
+    device_name_opt: Option<String>,
+    file_type: FileType,
+    stats: &mut FileStats
+) {
 
     // attach device name subdir path
     if let Some(device_name) = device_name_opt {
@@ -120,7 +173,7 @@ fn sort_file_to_subdir(file: DirEntry, target_subdir: &mut PathBuf, cwd_path: &P
     }
 
     // create target subdir
-    create_subdir_if_required(target_subdir, cwd_path);
+    create_subdir_if_required(target_subdir, cwd_path, stats);
 
     // attach file path
     // TODO 5: create new path variable?
@@ -128,35 +181,62 @@ fn sort_file_to_subdir(file: DirEntry, target_subdir: &mut PathBuf, cwd_path: &P
 
     // copy file
     // TODO 6a: move instead of copy
-    copy_file_if_not_exists(&file, target_subdir, cwd_path);
+    copy_file_if_not_exists(&file, target_subdir, cwd_path, file_type, stats);
 }
 
-fn copy_file_if_not_exists(file: &DirEntry, target_subdir: &PathBuf, path_cwd: &PathBuf) {
-    let file_copy_status = if target_subdir.exists() {
+fn copy_file_if_not_exists(
+    file: &DirEntry,
+    destination_path: &PathBuf,
+    path_cwd: &PathBuf,
+    file_type: FileType,
+    stats: &mut FileStats
+) {
+    let file_copy_status = if destination_path.exists() {
+        // println!("File already exists, skipping: {:?}", &file.file_name());
+
+        // Record stats for skipped files
+        match file_type {
+            FileType::Image   => stats.img_skipped(),
+            FileType::Video   => stats.vid_skipped(),
+            // don't record any stats for this, shouldn't get one here anyway
+            FileType::Unknown => ()
+        }
         "already exists"
+
     } else {
-        match fs::copy(file.path(), target_subdir) {
-            Ok(_) =>
-                "ok",
-            Err(err) =>
-                // println!("File copy error: {:?}: ERROR {:?}", &file.file_name(), err)
-                // TODO add error info
+        match fs::copy(file.path(), destination_path) {
+            Ok(_) => {
+                // Record stats for copied file
+                match file_type {
+                    FileType::Image   => stats.img_moved(),
+                    FileType::Video   => stats.vid_moved(),
+                    // don't record any stats for this, shouldn't get one here anyway
+                    FileType::Unknown =>()
+                }
+                "ok"
+            },
+            Err(err) => {
+                println!("File copy error: {:?}: ERROR {:?}", &file.file_name(), err);
+                // TODO 5c: log error info
+                stats.error_file_move();
                 "ERROR"
+            }
         }
     };
 
+    // TODO 3a/5c: maybe only log this?
     println!("Copying {} -> {} ... {}",
              // &file.file_name().to_str().unwrap(),
              file.path().strip_prefix(path_cwd).unwrap().display(),
              // &new_file_path.to_str().unwrap()),
-             target_subdir.strip_prefix(path_cwd).unwrap().display(),
+             destination_path.strip_prefix(path_cwd).unwrap().display(),
              file_copy_status);
 }
 
 // TODO 6b: path_cwd is only required for printlns
-fn create_subdir_if_required(target_subdir: &PathBuf, path_cwd: &PathBuf) {
+fn create_subdir_if_required(target_subdir: &PathBuf, path_cwd: &PathBuf, stats: &mut FileStats) {
     if target_subdir.exists() {
-        // TODO 2f: handle dir already exists, maybe just log it?
+        // TODO 5c: log dir already exists
         // println!("target dir exists: {}: {}",
         //          &target_subdir.strip_prefix(&path_cwd).unwrap().display(),
         //          &target_subdir.exists());
@@ -169,14 +249,17 @@ fn create_subdir_if_required(target_subdir: &PathBuf, path_cwd: &PathBuf) {
 
         match subdir_creation {
             Ok(_) => {
-                println!("(Created subdirectory {})",
+                stats.dirs_created();
+                println!("> created subdirectory {}",
                          target_subdir.strip_prefix(&path_cwd).unwrap().display());
             },
-            Err(e) =>
+            Err(e) => {
                 // TODO 2f: handle dir creation fail
+                stats.error_dir_creation();
                 println!("Failed to create subdirectory {}: {:?}",
                          target_subdir.strip_prefix(&path_cwd).unwrap().display(),
                          e.kind())
+            }
         }
     };
 }
@@ -246,7 +329,8 @@ fn get_device_name(file: &DirEntry) -> Option<String> {
 
         Err(e) => {
             // dbg!(e);
-            println!("Can not read EXIF for {:?}: {}", &file_name, e.to_string());
+            // TODO 5c: log this error?
+            println!("> can not read EXIF for {:?}: {}", file.file_name(), e.to_string());
             None
         }
     }
@@ -257,9 +341,9 @@ fn get_device_name(file: &DirEntry) -> Option<String> {
 fn read_exif_file<P: AsRef<Path>>(file_name: P) -> ExifResult {
     // let file_name = file_entry.path();
     let mut file = File::open(file_name).unwrap();
-    &file.seek(SeekFrom::Start(0)).unwrap();
+    let _ = &file.seek(SeekFrom::Start(0)).unwrap();
     let mut contents: Vec<u8> = Vec::new();
-    &file.read_to_end(&mut contents);
+    let _ = &file.read_to_end(&mut contents);
     let (res, _) = rexif::parse_buffer_quiet(&contents);
     res
 }
