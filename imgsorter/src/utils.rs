bc1aa938bbfdbbeab0a202f6125c3959b1081bae
cristan <13849765+cristan2@users.noreply.github.com>
2022-02-26 19:05:19 +0200

    Methods for formatting colored strings

---
+++ imgsorter/src/utils.rs
@@ -0,0 +1,31 @@
+
+pub struct ColoredString;
+
+/// Provides static methods for formatting colored text based on ANSI codes
+/// Taken from the following SO answers:
+/// * [https://stackoverflow.com/questions/69981449/how-do-i-print-colored-text-to-the-terminal-in-rust]
+/// * [https://stackoverflow.com/questions/287871/how-to-print-colored-text-to-the-terminal/287944#287944]
+impl ColoredString {
+
+    // Color codes:
+    // * MAGENTA = '\x1b[95m'
+    // * BLUE = '\x1b[94m'
+    // * CYAN = '\x1b[96m'
+    // * GREEN = '\x1b[92m'
+    // * ORANGE = '\x1b[93m'
+    // * RED = '\x1b[91m'
+    // * NO_COLOR = '\x1b[0m'
+    // * BOLD = '\x1b[1m'
+    // * UNDERLINE = '\x1b[4m'
+
+    pub fn magenta(s: &str) -> String { format!("\x1b[95m{}\x1b[0m", s) }
+    pub fn blue(s: &str) -> String { format!("\x1b[94m{}\x1b[0m", s) }
+    pub fn cyan(s: &str) -> String { format!("\x1b[96m{}\x1b[0m", s) }
+    pub fn green(s: &str) -> String { format!("\x1b[92m{}\x1b[0m", s) }
+    pub fn red(s: &str) -> String { format!("\x1b[91m{}\x1b[0m", s) }
+    pub fn no_color(s: &str) -> String { format!("\x1b[0m{}\x1b[0m", s) }
+    pub fn orange(s: &str) -> String { format!("\x1b[93m{}\x1b[0m", s) }
+    pub fn bold_white(s: &str) -> String { format!("\x1b[1m{}\x1b[0m", s) }
+    pub fn underline(s: &str) -> String { format!("\x1b[4m{}\x1b[0m", s) }
+}
+
